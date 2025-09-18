554976c9b99e53631e129476e0991e980949624d
shivk01 <shivksharma2332@gmail.com>
2025-09-18 23:33:52 +0530

    Map Study

--- src/DesignPatterns/Singleton.java
+++ src/DesignPatterns/Singleton.java
@@ -1,30 +1,50 @@
 package DesignPatterns;
 
-public class Singleton {
+import java.io.ObjectStreamException;
+import java.io.Serializable;
 
+public class Singleton implements Serializable
+{
+
+    private static final long serialVersionUID = 1L;
+
+    // volatile for double-checked locking
     private static volatile Singleton instance;
 
-    private Singleton() {}
+    private Singleton()
+    {
+        // Prevent reflection
+        if (instance != null)
+        {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
 
     public static Singleton getInstance()
     {
-        if(instance == null)
+        if (instance == null)
         {
             synchronized (Singleton.class)
             {
-                if(instance == null) instance = new Singleton();
+                if (instance == null)
+                    instance = new Singleton();
             }
         }
         return instance;
     }
 
-    public static void main(String[] args) {
+    // This method ensures the singleton after deserialization
+    protected Object readResolve() throws ObjectStreamException
+    {
+        return getInstance();
+    }
+
+    public static void main(String[] args)
+    {
         Singleton object1 = Singleton.getInstance();
         Singleton object2 = Singleton.getInstance();
         Singleton object3 = Singleton.getInstance();
-        System.out.println(object3 == object1);
 
+        System.out.println(object3 == object1);  // true
     }
-
-
 }
