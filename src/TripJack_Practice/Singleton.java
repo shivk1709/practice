2ff3309d88638703f501f705603800463466aa1f
shivk01 <shivksharma2332@gmail.com>
2025-10-24 23:55:39 +0530

    abcd

---
+++ src/TripJack_Practice/Singleton.java
@@ -0,0 +1,50 @@
+package TripJack_Practice;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public class Singleton implements Serializable
+{
+
+    private static final long serialVersionUID = 1L;
+
+    // volatile for double-checked locking
+    private static volatile Singleton instance;
+
+    private Singleton()
+    {
+        // Prevent reflection
+        if (instance != null)
+        {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            synchronized (Singleton.class)
+            {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+
+    // This method ensures the singleton after deserialization
+    protected Object readResolve() throws ObjectStreamException
+    {
+        return getInstance();
+    }
+
+    public static void main(String[] args)
+    {
+        Singleton object1 = Singleton.getInstance();
+        Singleton object2 = Singleton.getInstance();
+        Singleton object3 = Singleton.getInstance();
+
+        System.out.println(object3 == object1);  // true
+    }
+}
